871eff999089526c0e58ddf45b861fd43fc65785
Hannah Cutler New <Hannah-Cutler@Hannahs-MacBook-Pro-3.local>
2016-07-14 12:33:52 -0400

    cleaned up scripts

--- civicist/civ-link-parse.py
+++ civicist/civ-link-parse.py
@@ -7,23 +7,21 @@ import requests as rq
 import re
 
 #get contents of Civicist archives
-#base_url = "http://civichall.org/topic/first-post/"
 base_url = "http://civichall.org/civicist-archives/"
 
 r = rq.get(base_url)
 soup = bsoup(r.text, "lxml")
 
-# page_count_links = soup.find_all("a",href=re.compile(r".*javascript:goToPage.*"))
-# try: # Make sure there are more than one page, otherwise, set to 1.
-#     num_pages = int(page_count_links[-1].get_text())
-# except IndexError:
-#     num_pages = 1 #47 pages
+page_count_links = soup.find_all("a",href=re.compile(r".*javascript:goToPage.*"))
+try: # Make sure there are more than one page, otherwise, set to 1.
+    num_pages = int(page_count_links[-1].get_text())
+except IndexError:
+    num_pages = 48 #48 pages
 
-# url_list = ["{}page/{}".format(base_url, str(page)) for page in range(1, num_pages + 1)]
+url_list = ["{}page/{}".format(base_url, str(page)) for page in range(1, num_pages + 1)]
 url_list = []
 
-#url_list.remove('http://civichall.org/topic/first-post/page/1')
-#url_list.remove('http://civichall.org/civicist-archives/page/1')
+url_list.remove('http://civichall.org/civicist-archives/page/1')
 
 
 url_list.insert(0, "http://civichall.org/civicist-archives")
@@ -49,25 +47,5 @@ with open("articles_links-JULY.txt", "w") as outfile:
     outfile.write(x +'\n')
 
 
+# print article_links
 
-
-
-
-#print article_links
-
-
-# x = re.findall(r'(https?://\S+)', str(link))
-# print link[9:len(link)-15]
-
-  #get_article_links(html_doc)
-
-#get href links within post-title tags
-
-
-# def get_article_links(str):
-#   soup = BeautifulSoup(str, "lxml")
-
-
-
-# for url in url_list:
-#   html_doc = urllib2.urlopen(url).read()
--- techpresident/tp-link-parse.py
+++ techpresident/tp-link-parse.py
@@ -23,8 +23,6 @@ url_list = ["{}?page={}".format(base_url, str(page + 1054)) for page in range(1,
 #340 + 357 = 697
 #697 + 357 = 1054
 
-#url_list.remove("http://techpresident.com/blog?page=1") <- actual page - don't remove
-# url_list.insert(0, "http://techpresident.com/blog")
 
 article_links = []
 # loop through p. 341 ~ 1411 and grab partial article links
@@ -35,7 +33,6 @@ for url in url_list:
   
   #grab inner html with partial links
   for c in contents: #10 article links per page
-    # c = str(c)
     piece = c.find('h4')
     piece = str(piece)
 
@@ -55,45 +52,3 @@ for url in url_list:
 with open("techpres_links.txt", "w") as outfile:
   for x in article_links:
     outfile.write(x + '\n')
-
-
-#bad code - delete
-# base_url = "http://techpresident.com/topics/"
-# r = rq.get(base_url)
-# soup = bsoup(r.text, "lxml")
-
-# topics = ['revolution', 'obama-2012', 'malcolm-gladwell',
-#           'hacktivism', 'facebook', 'online-organizing',
-#           'wegov', 'backchannel', 'civic-hacking'] 
-
-# morelinks = ['http://techpresident.com/techpresident-topics/debates-20',
-#               'http://techpresident.com/techpresident-topics/hip-or-hype',
-#               'http://techpresident.com/techpresident-topics/grassrootsiness',
-#               'http://techpresident.com/techpresident-topics/email-watch',
-#               'http://techpresident.com/techpresident-topics/blogging',
-#               'http://techpresident.com/techpresident-topics/occupywallst',
-#               'http://techpresident.com/techpresident-topics/gop-2012',
-#               'http://techpresident.com/techpresident-topics/design']
-
-# links = []
-# for t in topics:
-#   url = base_url + t
-#   links.append(url)
-
-# for m in morelinks:
-#   links.append(m)
-
-# i = 0
-# for l in links:
-#   html_doc = urllib2.urlopen(l).read()
-
-#   fname = 'topicpage_' + str(i)
-#   i += 1
-
-#   with open(fname, "w") as html_file:
-#     html_file.write(html_doc)
-
-
-
-# contents = soup.findAll('div', attrs={"id": "topic-background"})
-# print contents
